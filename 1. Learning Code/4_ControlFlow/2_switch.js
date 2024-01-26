c8989d0f13143a8a7be60fb708aed896705fe17f
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2024-01-26 23:43:33 +0530

    Add files via upload

---
+++ 1. Learning Code/4_ControlFlow/2_switch.js
@@ -0,0 +1,42 @@
+const month =3
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("Default case match")
+        break;
+}
+
+//For string
+
+const months ="march"
+switch (months) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("Feb")
+        break;
+    case "march":
+        console.log("March")
+        break;
+    case "april":
+        console.log("April")
+        break;
+
+    default:
+        console.log("Default case match")
+        break;
+}
+
