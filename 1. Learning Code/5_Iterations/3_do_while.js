ee82b5d6dd25b3e25c51da108935c48d814dc407
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2024-01-27 23:16:58 +0530

    Add files via upload

---
+++ 1. Learning Code/5_Iterations/3_do_while.js
@@ -0,0 +1,14 @@
+//do_while loop = first do work then check the condition
+
+let score=1;
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
+
+console.log(`This is special case of Do_While loop`)
+let scoree=11;
+do {
+    console.log(`Score is ${scoree}`)
+    score++
+} while (scoree<=10);
