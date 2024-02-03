1478a0cff5e88e82f4e0fc2971c420835196e811
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2024-02-04 02:05:20 +0530

    Add files via upload

---
+++ 1. Learning Code/09_Asynchronous/Async Projects/Project5/5_script.js
@@ -0,0 +1,34 @@
+const insert=document.querySelector('#insert')
+window.addEventListener('keydown', (e)=>{
+    insert.innerHTML=`
+    <div class='color'>
+    <table>
+    <tr>
+        <td>Key</td>
+        <td>Keycode</td>
+        <td>Code </td>
+    </tr>
+    <tr>
+        <td>${e.key=== " "?"Space":e.key}</td>
+        <td>${e.keyCode}</td>
+        <td>${e.code}</td>
+    </tr>
+</table>
+    </div>
+    `
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
