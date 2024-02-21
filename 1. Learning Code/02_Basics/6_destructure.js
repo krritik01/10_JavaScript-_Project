f16ebb03d443390cec62f5e78764115f8be8fd1a
Ritik Kumar <98963769+krritik01@users.noreply.github.com>
2024-02-22 01:16:02 +0530

    Delete 1. Learning Code directory

--- 1. Learning Code/02_Basics/6_destructure.js
+++
@@ -1,4 +0,0 @@
-let arr = [3,4,5,6,7]
-let [first, second, third, fourth, fifth] = arr
-console.log(first)
-console.log(first, second, third)
--- 1. Learning Code/11_Promises/2_promises.js
+++
@@ -1,9 +0,0 @@
-//Other way to make promises
-new Promise((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Async task 2")
-        resolve()
-    }, 1000);
-}).then(() => {
-    console.log("Async 2 resolved")
-})
